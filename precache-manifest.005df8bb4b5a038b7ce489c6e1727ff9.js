06e93c6d40241d72f1cc23509c1ae725ddde3719
Sander <cooladress@mail.ru>
2019-11-20 22:23:03 +0300

    Updates

---
+++ precache-manifest.005df8bb4b5a038b7ce489c6e1727ff9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "920bc328bf9aa7536d6da86e597a20da",
+    "url": "/exchange-app/index.html"
+  },
+  {
+    "revision": "7f1c61931e1ebcca56d7",
+    "url": "/exchange-app/static/css/main.fa6b97c7.chunk.css"
+  },
+  {
+    "revision": "1bdc9219abaf9c0299ac",
+    "url": "/exchange-app/static/js/2.8ba5d986.chunk.js"
+  },
+  {
+    "revision": "7f1c61931e1ebcca56d7",
+    "url": "/exchange-app/static/js/main.caf5c004.chunk.js"
+  },
+  {
+    "revision": "e042c7ebc458e8a5f20a",
+    "url": "/exchange-app/static/js/runtime-main.900b10db.js"
+  }
+]);
